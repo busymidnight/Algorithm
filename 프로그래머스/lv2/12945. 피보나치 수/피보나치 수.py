dd94e889dde72a02a9c07b1db97bc7c175b8230d
busymidnight <100250055+busymidnight@users.noreply.github.com>
2023-04-01 19:16:06 +0900

    [level 2] Title: 피보나치 수, Time: 467.92 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,8 +1,6 @@
 def solution(n):
     d = [0] *100001
-    d[1] = 1
-    d[2] = 1
+    d[1], d[2] = 1, 1
     for i in range(3, n+1):
-        d[i] = d[i-1] + d[i-2]
-    answer = d[n] % 1234567   
-    return answer
+        d[i] = d[i-1] + d[i-2]   
+    return d[n] % 1234567
