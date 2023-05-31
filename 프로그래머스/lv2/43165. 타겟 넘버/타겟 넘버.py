7d557ee30c0e109d49f06b9764e81d2cf80ba433
busymidnight <100250055+busymidnight@users.noreply.github.com>
2023-06-01 08:27:40 +0900

    [level 2] Title: 타겟 넘버, Time: 3.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -3,11 +3,11 @@ def solution(numbers, target):
     def dfs(n, sum, numbers,target):
         nonlocal answer
         if n == len(numbers):
-                if sum == target:
+                if sum == target: #타겟 넘버 만드는 방법의 수 증가
                     answer += 1
                 return
-        dfs(n+1,sum+numbers[n], numbers, target)
-        dfs(n+1,sum-numbers[n], numbers, target)
+        dfs(n+1,sum+numbers[n], numbers, target) #먼저 하나씩 더한다
+        dfs(n+1,sum-numbers[n], numbers, target) #다음에 하나씩 뺀다
     
     answer = 0
     dfs(0,0,numbers,target)
