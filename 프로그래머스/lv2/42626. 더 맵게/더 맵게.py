afb1bbaddfd395e52fd0e10c62b89de9c7ab82be
busymidnight <100250055+busymidnight@users.noreply.github.com>
2023-06-22 23:24:29 +0900

    [level 2] Title: 더 맵게, Time: 2037.83 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -1,15 +1,12 @@
 import heapq
-
-def solution(S, K):
-    heap = []
-    for i in S:
-        heapq.heappush(heap, i)
-
-    cnt = 0
-    while heap[0] < K:
-        heapq.heappush(heap, heapq.heappop(heap) + heapq.heappop(heap) * 2)
-        cnt += 1
-        
-        if len(heap) == 1 and heap[0] < K:
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while scoville[0]<K:
+        new_s = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
+        heapq.heappush(scoville,new_s)
+        answer += 1
+        if scoville[0]<K and len(scoville)==1:
             return -1
-    return cnt
+
+    return answer
