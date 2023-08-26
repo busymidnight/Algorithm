68c7ddd62de387d4e0632662180570db831f3cc0
YK Park <busymidnight17@gmail.com>
2023-08-26 16:48:28 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4943.55 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -3,8 +3,10 @@ def solution(elements):
     #원형 수열 = 연속된 수열 = 원소를 두 개씩
     cycle = elements * 2
     i = 0
+    elem_sum = 0
     for i in range(len(elements)):
         for j in range(len(elements)):
-            answer.append(sum(cycle[i:i+j]))
+            elem_sum = sum(cycle[i:i+j])
+            answer.append(elem_sum)
         
     return len(set(answer))
